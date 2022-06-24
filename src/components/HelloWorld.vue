5fff2c010cad2d70fe399aff0bfe0ed5b4c5da7d
Yvan <i@zhuyunfeng.com>
2022-06-24 16:22:16 +0800

    :tada: first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Author: Yvan i@zhuyunfeng.com
+ * @Date: 2022-06-24 13:52:48
+ * @LastEditors: Yvan i@zhuyunfeng.com
+ * @LastEditTime: 2022-06-24 14:03:51
+ * @FilePath: \flip-clock\src\components\HelloWorld.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: red;
+}
+</style>
